43220c3fae93a95b27ccfdc36838de85af79ebf3
frontdevuz <davlatovizzatillo7@gmail.com>
2023-09-27 16:10:45 +0500

    Add react-router dom and pages !

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+
+//Importing pages for web application
+import Home from "./pages/home/home";
+import About from "./pages/about/about";
+import Service from "./pages/service/service";
+import Contact from "./pages/contact/contact";
+import Blog from "./pages/blog/blog";
+import Navbar from "./components/navbar/navbar";
+import Footer from "./components/footer/footer";
 
 export default function App() {
   return (
-    <div>
-      
-    </div>
-  )
+    <React.Fragment>
+      <Navbar />
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/service" element={<Service />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+      <Footer />
+    </React.Fragment>
+  );
 }
---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum,
+      similique tempore magni ut consectetur doloremque perspiciatis, quod
+      cumque aut fuga in debitis itaque rem, vitae libero ex veritatis odit
+      molestiae.
+    </div>
+  );
+}
