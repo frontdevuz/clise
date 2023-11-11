e12d62d1139b96e0fa19caf3ecf3af659735ec1d
frontdevuz <davlatovizzatillo7@gmail.com>
2023-11-11 12:11:39 +0500

    main

--- src/data/if-service-data.js
+++ src/data/if-service-data.js
@@ -3,19 +3,19 @@ export const Servi = [
     id: 1,
     class: " fa-code",
     title: "Front end",
-    text: "Zamonaviy veb ilovalarning tashqi ko'rinishini   qurish.",
+    text: "Zamonaviy veb ilovalarning tashqi ko'rinishini   qurish",
   },
   {
     id: 2,
     class: " fa-code",
     title: "Back end",
-    text: "Zamonaviy veb ilovalarning ma'lumot qismi ya'ni miyyasini  qurish.",
+    text: "Zamonaviy veb ilovalarning ma'lumot qismi ya'ni miyyasini  qurish",
   },
   {
     id: 3,
     className: " fa-code",
     title: "Flutter",
-    text: "Android va iOS (iPhone qurilmalari uchun zamonaviy ilovalar qilish ).",
+    text: "Android va iOS (iPhone qurilmalari uchun zamonaviy ilovalar qilish )",
   },
   {
     id: 4,
